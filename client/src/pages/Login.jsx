37f4e76730070362dc2c4245ae6932ecbf838b4d
Pawani <pawani02jp@gmail.com>
2025-10-06 20:06:34 +0530

    Initialized backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+   const [inputs, setInputs] = useState({
+    username:"",
+    password:"",
+  });
+
+  const [err, setErr] = useState(null)
+
+  const navigate = useNavigate()
+
+  const handleChange = e =>{
+      setInputs(prev=>({...prev, [e.target.name]: e.target.value}))
+  }
+
+  const handleSubmit = async e =>{
+
+      e.preventDefault()
+      try{
+         const res = await axios.post("/api/auth/login", inputs)
+         navigate("/");
+      }
+       catch(err){
+          setErr(err.response.data)
+       }
+  }
+
   return (
     <div className='auth'>
       <h1>Login</h1>
       <form>
-        <input type="text" placeholder='username' />
-        <input type="password" placeholder='password' />
-        <button>Login</button>
-        <p> This is an Error!</p>
+        <input type="text" placeholder='username' name="username" onChange={handleChange} />
+        <input type="password" placeholder='password' name="password" onChange={handleChange} />
+        <button onClick={handleSubmit}>Login</button>
+        {err && <p> {err}</p>}
         <span>Don't have an account? <a href="/register">Register</a></span>
       </form>
       
